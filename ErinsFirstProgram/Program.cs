852f9d015664b9a82b7259ae104e7e14e655b34e
Caleb Curry <clbcrry@hotmail.com>
2016-07-30 10:04:43 -0400

    Initial Commit - Takes date and calculates how many days old you are

---
+++ ErinsFirstProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErinsFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Year of Birth: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Month of Birth: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Day of Birth: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            
+            DateTime birthDate = new DateTime(year, month, day);
+
+            CalculateDaysSinceBirth(birthDate);
+            
+        }
+       static void CalculateDaysSinceBirth(DateTime birthDate)
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan daysAlive = now - birthDate;
+            Console.WriteLine(daysAlive.Days);
+        }
+    }
+}
